48ea208fac0502883707a79b34a9630b4644aac8
captain82 <nktkumar82@gmail.com>
2025-09-26 22:51:15 +0530

    Translucent code reverted

--- Sources/Translucent/Translucent.swift
+++ Sources/Translucent/Translucent.swift
@@ -68,131 +68,105 @@ public class Wallpaper: NSObject {
             heightKey += "x"
         }
         
-        // Debug info to help diagnose iPhone 15 cropping issues
-        print("Device: \(Device.current)")
-        print("Screen height: \(height)")
-        print("HeightKey: \(heightKey)")
-        print("iOS Version: \(UIDevice.current.systemVersion)")
-        print("iOS 18 or higher: \(isIOS18OrHigher)")
-        
         guard let phone = phoneMappings["\(heightKey)"] as? PhoneModel else {
             print("It looks like you selected an image that isn't an iPhone screenshot, or your iPhone is not supported. Try again with a different image.")
             return nil
         }
         
+        var crop_x:CGFloat = 0
+        var crop_y:CGFloat = 0
+        var crop_w:CGFloat = 0
+        var crop_h:CGFloat = 0
+
         // Select the appropriate phone properties based on the widgetType
         let selectedPhone: Phone
         
         if isIOS18OrHigher {
             // For iOS 18 or higher, use the text or notext properties
             selectedPhone = widgetType == 1 ? phone.text : (phone.notext ?? phone.text)
-            print("iOS 18 or higher - Using \(widgetType == 1 ? "text" : "notext") mappings")
-            print("Selected phone coordinates - small: \(selectedPhone.small), left: \(selectedPhone.left), right: \(selectedPhone.right), top: \(selectedPhone.top), middle: \(selectedPhone.middle), bottom: \(selectedPhone.bottom)")
+            print("iOS 18 or higher")
+            print(selectedPhone.small)
         } else {
             selectedPhone = phone.text
-            print("Lower than iOS 18 - Using text mappings")
-            print("Selected phone coordinates - small: \(selectedPhone.small), left: \(selectedPhone.left), right: \(selectedPhone.right), top: \(selectedPhone.top), middle: \(selectedPhone.middle), bottom: \(selectedPhone.bottom)")
+            print("Lower than iOS 18")
+        }
+          
+                
+        switch position{
+        case .smallTopLeft:
+            crop_w = selectedPhone.small
+            crop_h = selectedPhone.small
+            crop_x = selectedPhone.left
+            crop_y = selectedPhone.top
+            
+        case .smallTopRight:
+            crop_w = selectedPhone.small
+            crop_h = selectedPhone.small
+            crop_x = selectedPhone.right
+            crop_y = selectedPhone.top
+            
+        case .smallCenterLeft:
+            crop_w = selectedPhone.small
+            crop_h = selectedPhone.small
+            crop_x = selectedPhone.left
+            crop_y = selectedPhone.middle
+            
+        case .smallCenterRight:
+            crop_w = selectedPhone.small
+            crop_h = selectedPhone.small
+            crop_x = selectedPhone.right
+            crop_y = selectedPhone.middle
+            
+        case .smallBottomLeft:
+            crop_w = selectedPhone.small
+            crop_h = selectedPhone.small
+            crop_x = selectedPhone.left
+            crop_y = selectedPhone.bottom
+            
+        case .smallBottomRight:
+            crop_w = selectedPhone.small
+            crop_h = selectedPhone.small
+            crop_x = selectedPhone.right
+            crop_y = selectedPhone.bottom
+            
+        case .mediumTop:
+            crop_w = selectedPhone.medium
+            crop_h = selectedPhone.small
+            crop_x = selectedPhone.left
+            crop_y = selectedPhone.top
+            
+        case .mediumCenter:
+            crop_w = selectedPhone.medium
+            crop_h = selectedPhone.small
+            crop_x = selectedPhone.left
+            crop_y = selectedPhone.middle
+            
+        case .mediumBottom:
+            crop_w = selectedPhone.medium
+            crop_h = selectedPhone.small
+            crop_x = selectedPhone.left
+            crop_y = selectedPhone.bottom
+            
+        case .largeTop:
+            crop_w = selectedPhone.medium
+            crop_h = selectedPhone.large
+            crop_x = selectedPhone.left
+            crop_y = selectedPhone.top
+            
+        case .largeBottom:
+            crop_w = selectedPhone.medium
+            crop_h = selectedPhone.large
+            crop_x = selectedPhone.left
+            crop_y = selectedPhone.middle
         }
         
-        // Improved cropping logic based on reference implementation
-        let crop = calculateCropDimensions(for: position, using: selectedPhone)
-        
-        print("Final crop coordinates - x: \(crop.x), y: \(crop.y), width: \(crop.w), height: \(crop.h)")
-        print("Widget position: \(position)")
-        
-        let croppedCGImage = wallpaperImage.cgImage?.cropping(to: CGRect(x: crop.x, y: crop.y, width: crop.w, height: crop.h))
-        let croppedImage = UIImage(cgImage: croppedCGImage!)
+        let crop = wallpaperImage.cgImage?.cropping(to: CGRect(x: crop_x, y: crop_y, width: crop_w, height: crop_h))
+        let croppedImage = UIImage(cgImage: crop!)
         
         return croppedImage
             
     }
-    
-    // Helper function to calculate crop dimensions using improved logic from reference implementation
-    private func calculateCropDimensions(for position: WidgetCropPosition, using phone: Phone) -> CropDimensions {
-        // Determine widget size category
-        let widgetSize: WidgetSize
-        let verticalPosition: VerticalPosition
-        let horizontalPosition: HorizontalPosition?
-        
-        switch position {
-        case .smallTopLeft, .smallTopRight, .smallCenterLeft, .smallCenterRight, .smallBottomLeft, .smallBottomRight:
-            widgetSize = .small
-        case .mediumTop, .mediumCenter, .mediumBottom:
-            widgetSize = .medium
-        case .largeTop, .largeBottom:
-            widgetSize = .large
-        }
-        
-        // Extract position components
-        switch position {
-        case .smallTopLeft, .mediumTop, .largeTop:
-            verticalPosition = .top
-            horizontalPosition = .left
-        case .smallTopRight:
-            verticalPosition = .top
-            horizontalPosition = .right
-        case .smallCenterLeft, .mediumCenter:
-            verticalPosition = .middle
-            horizontalPosition = .left
-        case .smallCenterRight:
-            verticalPosition = .middle
-            horizontalPosition = .right
-        case .smallBottomLeft, .mediumBottom:
-            verticalPosition = .bottom
-            horizontalPosition = .left
-        case .smallBottomRight:
-            verticalPosition = .bottom
-            horizontalPosition = .right
-        case .largeBottom:
-            verticalPosition = .middle
-            horizontalPosition = .left
-        }
-        
-        // Calculate dimensions using the reference logic
-        let w: CGFloat = (widgetSize == .small) ? phone.small : phone.medium
-        let h: CGFloat = (widgetSize == .large) ? phone.large : phone.small
-        
-        let x: CGFloat
-        if widgetSize == .small {
-            x = (horizontalPosition == .left) ? phone.left : phone.right
-        } else {
-            x = phone.left  // Medium and large widgets always use left position
-        }
-        
-        let y: CGFloat
-        switch verticalPosition {
-        case .top:
-            y = phone.top
-        case .middle:
-            y = phone.middle
-        case .bottom:
-            y = phone.bottom
-        }
-        
-        return CropDimensions(x: x, y: y, w: w, h: h)
-    }
 }
-
-// Helper structs for improved crop logic
-struct CropDimensions {
-    let x: CGFloat
-    let y: CGFloat
-    let w: CGFloat
-    let h: CGFloat
-}
-
-enum WidgetSize {
-    case small, medium, large
-}
-
-enum VerticalPosition {
-    case top, middle, bottom
-}
-
-enum HorizontalPosition {
-    case left, right
-}
-
 struct Phone: Codable {
     var small:  CGFloat
     var medium: CGFloat
